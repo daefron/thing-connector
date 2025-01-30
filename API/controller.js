48613bcb3ce00e86b890bc73299d427f97c3fc6b
Daefron <thomas_evans@outlook.com>
2025-01-31 09:18:04 +1100

    Added server timeout handling

--- API/controller.js
+++ API/controller.js
@@ -7,17 +7,29 @@ const model = genAI.getGenerativeModel(aiConfig.model);
 
 async function postData(req, res) {
   const userInput = req.body.userInput;
-  console.log("Getting request with input: '" + userInput + "'");
   const prompt = aiConfig.prompt + userInput;
+
+  //attemps to generate response multiple times to avoid server timeout
   let result;
-  try {
-    result = await model.generateContent(prompt);
-  } catch (e) {
-    console.log("Error: " + e.status);
-    return res.status(e.status).send({ message: e.status });
+  const maxAttempts = 5;
+  let attempts = 0;
+  tryGenerate();
+  async function tryGenerate() {
+    try {
+      console.log("Getting request with input: '" + userInput + "'");
+      result = await model.generateContent(prompt);
+    } catch (e) {
+      console.log("Error: " + e.status);
+      if (attempts < maxAttempts) {
+        return tryGenerate();
+      }
+      return res.status(e.status).send({ message: e.status });
+    }
   }
+  
   const parsedResult = JSON.parse(result.response.text());
-  res.json({ result: parsedResult });
+  console.log("Sent result for: '" + userInput + "'");
+  return res.json({ result: parsedResult });
 }
 
 module.exports = {
