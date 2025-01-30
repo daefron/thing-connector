bdd501bf9f211a0b29ada41dbb58355434f15e57
Daefron <thomas_evans@outlook.com>
2025-01-30 16:51:58 +1100

    Modified AI prompt to be more exact

--- API/aiConfig.js
+++ API/aiConfig.js
@@ -33,7 +33,7 @@ const model = {
 };
 
 const prompt =
-  "Give me the top 10 shared connections between the following things (considering all variants of the words, showing percentage for each connection's strength and explanation of each connection, sorted from highest to lowest percentage): ";
+  "Give me the top 10 shared connections between the following things (considering all word variants, showing percentage for each connection's strength and explanation of each connection, sorted from highest to lowest percentage, connections must apply to all things): ";
 
 module.exports = {
   model,
